c15ee91e6033b5541cc54f3ae62ca511540e2fa2
chandresh <chandresh.maheshwari@gmail.com>
2025-08-08 11:08:51 +0530

    changes for got to my account in admin panel

--- src/Wa-Frontend/NavLayout.js
+++ src/Wa-Frontend/NavLayout.js
@@ -165,6 +165,14 @@ const Navlayout = () => {
         navigate('/');
     };
 
+     const handleMyAccount = () => {
+          const userData = localStorage.getItem("userData");
+      const dynamicHost = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+      const token = localStorage.getItem("WAauthToken");
+      window.location.href = `${dynamicHost}/admin/user/dashboard/?token=${token}`;
+        // navigate('/');
+    };
+
     // const handleDashboard = async () => {
     //     const dynamicHost = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
     //     const token = localStorage.getItem("WAauthToken");
@@ -293,6 +301,7 @@ const Navlayout = () => {
                                         <div className="dropdown-menu">
                                             <button onClick={handleViewProfile}>View Profile</button>
                                             <button onClick={handleEditProfile}>Edit Profile</button>
+                                            <button onClick={handleMyAccount}>My Account</button>
                                             <button onClick={handleLogout}>Logout</button>
                                         </div>
                                     )}
